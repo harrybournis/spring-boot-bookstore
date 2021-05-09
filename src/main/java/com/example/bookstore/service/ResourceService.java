21a77948b296708723f5611fd447b2b650ce7d9b
Harry Bournis <harrybournis@gmail.com>
2021-05-09 13:55:57 +0300

    fix interface

--- src/main/java/com/example/bookstore/service/ResourceService.java
+++ src/main/java/com/example/bookstore/service/ResourceService.java
@@ -3,11 +3,12 @@ package com.example.bookstore.service;
 import com.example.bookstore.exception.ApiException;
 import com.example.bookstore.exception.ResourceNotFoundException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public interface ResourceService<T> {
   List<T> getAll();
   T find(Long id) throws ResourceNotFoundException;
-  T save(T resource) throws ApiException;
+  T save(@Valid T resource) throws ApiException;
   void delete(T resource);
 }
